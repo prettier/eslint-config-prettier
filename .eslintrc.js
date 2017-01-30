5a4afe6962a21cd726828e900d7c462a59c57f8f
Simon Lydell <simon.lydell@gmail.com>
2017-01-30 08:46:11 +0100

    Replace index.js with "main" in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ["google", "plugin:react/all", "./index.js", "./rules/react.js"],
+  extends: [
+    "google",
+    "plugin:react/all",
+    "./rules/core.js",
+    "./rules/react.js"
+  ],
   plugins: ["react", "prettier"],
   env: { es6: true, node: true },
   rules: { "require-jsdoc": "off", "prettier/prettier": "error" }
