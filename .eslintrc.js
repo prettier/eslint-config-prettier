cb683d6a9d9d6d845e911f6b8b808b6c3c87c625
Simon Lydell <simon.lydell@gmail.com>
2017-01-31 07:52:44 +0100

    Upgrade prettier (devDependency)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,12 @@ module.exports = {
     "./rules/react.js"
   ],
   plugins: ["react", "prettier"],
-  env: { es6: true, node: true },
-  rules: { "require-jsdoc": "off", "prettier/prettier": "error" }
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    "require-jsdoc": "off",
+    "prettier/prettier": "error"
+  }
 };
