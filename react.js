26c6bd56b938b91e9ca1066ed8297c160d739961
Simon Lydell <simon.lydell@gmail.com>
2017-02-09 08:09:17 +0100

    Fix react rules
    I had forgotten to prefix them with `react/` -.-
    
    Apparently ESLint does not warn about unknown rules when they are set to
    `0` or `"off"`.

--- react.js
+++ react.js
@@ -7,15 +7,15 @@
 
 module.exports = {
   rules: {
-    "jsx-closing-bracket-location": "off",
-    "jsx-curly-spacing": "off",
-    "jsx-equals-spacing": "off",
-    "jsx-first-prop-new-line": "off",
-    "jsx-indent": "off",
-    "jsx-indent-props": "off",
-    "jsx-max-props-per-line": "off",
-    "jsx-space-before-closing": "off",
-    "jsx-tag-spacing": "off",
-    "jsx-wrap-multilines": "off"
+    "react/jsx-closing-bracket-location": "off",
+    "react/jsx-curly-spacing": "off",
+    "react/jsx-equals-spacing": "off",
+    "react/jsx-first-prop-new-line": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-max-props-per-line": "off",
+    "react/jsx-space-before-closing": "off",
+    "react/jsx-tag-spacing": "off",
+    "react/jsx-wrap-multilines": "off"
   }
 };
