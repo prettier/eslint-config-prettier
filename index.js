7266163e7e3d8a6c35683f42803f3958bbb85a49
Simon Lydell <simon.lydell@gmail.com>
2017-11-01 19:34:12 +0100

    Actually mark lines-around-comment as a special rule

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
     // information. (These are marked with `0` instead of `"off"` so that a
     // script can distinguish them.)
     curly: 0,
+    "lines-around-comment": 0,
     "max-len": 0,
     "no-confusing-arrow": 0,
     "no-mixed-operators": 0,
@@ -34,7 +35,6 @@ module.exports = {
     "jsx-quotes": "off",
     "key-spacing": "off",
     "keyword-spacing": "off",
-    "lines-around-comment": "off",
     "multiline-ternary": "off",
     "newline-per-chained-call": "off",
     "new-parens": "off",
