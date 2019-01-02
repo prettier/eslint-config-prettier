6ec7e8e6fb24ee79ec45a241ee9e2fda42450fb0
michalsnik <msajnog93@gmail.com>
2019-01-02 14:57:41 +0100

    chore(vue): Use dedicated vue/no-layout-rules config

--- vue.js
+++ vue.js
@@ -1,20 +1,8 @@
 "use strict";
 
 module.exports = {
+  extends: "plugin:vue/no-layout-rules",
   rules: {
-    "vue/html-self-closing": 0,
-
-    "vue/html-closing-bracket-newline": "off",
-    "vue/html-closing-bracket-spacing": "off",
-    "vue/html-end-tags": "off",
-    "vue/html-indent": "off",
-    "vue/html-quotes": "off",
-    "vue/max-attributes-per-line": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/mustache-interpolation-spacing": "off",
-    "vue/no-multi-spaces": "off",
-    "vue/no-spaces-around-equal-signs-in-attribute": "off",
-    "vue/script-indent": "off",
-    "vue/singleline-html-element-content-newline": "off"
+    "vue/html-self-closing": 0
   }
 };
