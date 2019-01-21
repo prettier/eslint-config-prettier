fc4d30afdc850324b71bc0335ad3ac2bbb6be92d
TANIGUCHI Masaya <xtaniguchimasaya+code@gmail.com>
2019-01-21 11:17:03 +0900

    Add @typescript-eslint rules

--- typescript.js
+++ typescript.js
@@ -4,6 +4,9 @@ module.exports = {
   rules: {
     "typescript/indent": "off",
     "typescript/member-delimiter-style": "off",
-    "typescript/type-annotation-spacing": "off"
+    "typescript/type-annotation-spacing": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/type-annotation-spacing": "off"
   }
 };
