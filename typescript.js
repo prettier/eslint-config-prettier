531bb73adbb13c20de0c52bb9821045e7ffc3b40
Daniel Tschinder <daniel@tschinder.de>
2019-01-24 13:06:06 -0800

    Support @typescript-eslint/eslint-plugin
    The plugin got moved to a new namespaces module.

--- typescript.js
+++ typescript.js
@@ -3,7 +3,10 @@
 module.exports = {
   rules: {
     "typescript/indent": "off",
+    "@typescript-eslint/indent": "off",
     "typescript/member-delimiter-style": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
     "typescript/type-annotation-spacing": "off"
+    "@typescript-eslint/type-annotation-spacing": "off",
   }
 };
