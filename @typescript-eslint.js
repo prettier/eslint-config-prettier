7328392bff2bc21f9eea39a7933ac3fd84db33e0
Simon Lydell <simon.lydell@gmail.com>
2019-05-16 09:53:58 +0200

    Disable new @typescript-eslint rules
    Fixes #91.

--- @typescript-eslint.js
+++ @typescript-eslint.js
@@ -2,9 +2,11 @@
 
 module.exports = {
   rules: {
+    "@typescript-eslint/func-call-spacing": "off",
     "@typescript-eslint/indent": "off",
     "@typescript-eslint/member-delimiter-style": "off",
     "@typescript-eslint/no-extra-parens": "off",
+    "@typescript-eslint/semi": "off",
     "@typescript-eslint/type-annotation-spacing": "off"
   }
 };
