e2472cb1fd61e183da5b209c4a2726ecbd9f62c7
JounQin <admin@1stg.me>
2025-03-07 15:03:07 +0800

    feat: migrate to exports field

--- prettier.js
+++ prettier.js
@@ -1,12 +1,10 @@
 "use strict";
 
-module.exports = {
-  rules: {
-    // These are safe to use as long as the `"prettier/prettier"` rule from
-    // eslint-plugin-prettier isn’t enabled.
-    // These are also included in `"plugin:prettier/recommended"`:
-    // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
-    "arrow-body-style": 0,
-    "prefer-arrow-callback": 0,
-  },
+exports.rules = {
+  // These are safe to use as long as the `"prettier/prettier"` rule from
+  // eslint-plugin-prettier isn’t enabled.
+  // These are also included in `"plugin:prettier/recommended"`:
+  // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+  "arrow-body-style": 0,
+  "prefer-arrow-callback": 0,
 };
